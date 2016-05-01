12034b55a0bb4e3ad5394f21d8bc48a75cadfe7d
Reina Mercedes <Reina Mercedes>
2016-05-01 12:59:33 -0500

    avance

---
+++ New Unity Project/Assets/changesize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changesize : MonoBehaviour {
+	public GameObject imagen;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void cambiartamaño(){
+		imagen.transform.localScale = new Vector2 (2f, 2f);
+	}
+}
